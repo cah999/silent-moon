89137eac3ae80b40783308cb33d439ffb523db8d
Alexander <73055263+cah999@users.noreply.github.com>
2024-03-09 12:46:43 +0700

    small screen hotfix

--- app/src/main/java/com/example/silentmoon/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/silentmoon/ui/profile/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.silentmoon.R
 import com.example.silentmoon.databinding.FragmentProfileBinding
 import com.example.silentmoon.ui.main.MainActivity
 
@@ -21,11 +22,21 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(getString(R.string.mysharedpref), MODE_PRIVATE)
+        var name = sharedPreferences.getString(
+            getString(R.string.name),
+            getString(R.string.default_nickname)
+        )
+        if (name != null) {
+            if (name.isEmpty()) {
+                name = getString(R.string.default_nickname)
+            }
+        }
+        binding.profileNickname.text = name
+
         binding.logoutBtn.setOnClickListener {
-            val sharedPreferences = requireActivity().getSharedPreferences(
-                getString(com.example.silentmoon.R.string.mysharedpref),
-                MODE_PRIVATE
-            )
             val editor = sharedPreferences.edit()
             editor.clear()
             editor.apply()
